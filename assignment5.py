35d3c0aec3b843aabb518ec87ae7335f6b244d22
fubao <freadream@gmail.com>
2017-10-13 22:14:35 -0400

    update

--- assignment5.py
+++ assignment5.py
@@ -2,14 +2,17 @@ import numpy as np
 from scipy.special import erfinv
 import unittest
 import matplotlib.pyplot as plt
-
+import math
 ###########################################
 # Problem 1
 ###########################################
 
 def inverse_normal_cdf(u,mu,sigma2):
     ## Add code here ##
-    return -1
+    
+    inverseError= erfinv(2*u-1)               #inverse erf function
+    res = mu + math.sqrt(2*sigma2)*inverseError
+    return res
     
 ###########################################
 # Problem 2
@@ -17,8 +20,15 @@ def inverse_normal_cdf(u,mu,sigma2):
 
 def normal_sampler(n_samples,mu,sigma2):
     ## Add code here ##
-    return -1
     
+    u = np.random.uniform(0, 1, n_samples)             #sample from uniform first
+    #print ("u: ",u)
+    
+    normalX= inverse_normal_cdf(u, mu, sigma2)
+    plt.hist(normalX, normed=True, bins=30)
+    plt.xlabel('x');
+    plt.ylabel('Probability of normal distribution');
+    plt.show() 
 ###########################################
 # Problem 3
 ###########################################
@@ -26,3 +36,9 @@ def normal_sampler(n_samples,mu,sigma2):
 def mcev(fun,n_samples,mu,sigma2):
     ## Add code here ##
     return -1
+
+
+
+##########test############
+    
+normal_sampler(1000,0, 2)
