0d05b7093e14692f95101aed91cf3d1e43e9cb5b
fubao <freadream@gmail.com>
2017-10-13 23:58:59 -0400

    update

--- assignment5.py
+++ assignment5.py
@@ -8,8 +8,7 @@ import math
 ###########################################
 
 def inverse_normal_cdf(u,mu,sigma2):
-    ## Add code here ##
-    
+    #get X from inverse normal CDF 
     inverseError= erfinv(2*u-1)               #inverse erf function
     res = mu + math.sqrt(2*sigma2)*inverseError
     return res
@@ -19,26 +18,36 @@ def inverse_normal_cdf(u,mu,sigma2):
 ###########################################
 
 def normal_sampler(n_samples,mu,sigma2):
-    ## Add code here ##
-    
+    #normal distribution use inverse CDF
     u = np.random.uniform(0, 1, n_samples)             #sample from uniform first
-    #print ("u: ",u)
     
     normalX= inverse_normal_cdf(u, mu, sigma2)
     plt.hist(normalX, normed=True, bins=30)
     plt.xlabel('x');
-    plt.ylabel('Probability of normal distribution');
-    plt.show() 
+    plt.ylabel('Count');
+    plt.title('Histogram of samples for normal distribution')
+    plt.show(block = False) 
+    
+    return normalX
+
 ###########################################
 # Problem 3
 ###########################################
 
 def mcev(fun,n_samples,mu,sigma2):
-    ## Add code here ##
-    return -1
+    ## estimated function fun's expected value by monte carlo simulation
+    sampleX =  normal_sampler(n_samples, mu, sigma2)
 
+    fx = fun(sampleX)    
+    print ("fx: ", type(fx))     #get CDF
+    return np.ndarray.mean(fx)
 
 
 ##########test############
     
-normal_sampler(1000,0, 2)
+#normal_sampler(1000,0, 2)
+
+def fun(x):
+    return x**2      # x, x**3
+
+print ("test mcev", mcev(fun, 1000, 0.0, 2.0))
